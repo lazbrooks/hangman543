cba068e900224b7e141e32a44c814e78101faaba
lazbrooks <ltrbrooks@gmail.com>
2023-11-29 21:58:47 +0000

    Implements the objectives of tasks 2-4.

--- milestone_2.py
+++ milestone_2.py
@@ -1,2 +1,14 @@
+import random
+
 word_list = ["blimp", "blemish","perhaps", "glorify", "susurrus"]
 print(word_list)
+
+word = random.choice(word_list)
+print(word)
+
+guess = input("Please choose a letter: ")
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
