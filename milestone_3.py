407481a4da7143ac92449d6cf63ff564a1857273
lazbrooks <ltrbrooks@gmail.com>
2023-11-30 19:30:05 +0000

    Updates milestone_3.py as instructed in Tasks 3 and 4.

--- milestone_3.py
+++ milestone_3.py
@@ -1,22 +1,30 @@
 import random
 
+# Instate a list of words for the computer to pick from randomnly and the user to guess.
 word_list = ["blimp", "blemish","perhaps", "glorify", "susurrus"]
-print(word_list)
+# print(word_list)
 
 word = random.choice(word_list)
 print(word)
 
-guess = input("Please choose a letter: ")
 
-# if len(guess) == 1 and guess.isalpha():
-#     print("Good guess!")
-# else:
-#     print("Oops! That is not a valid input.")
+def ask_for_input():
+# Gather a user unput and test it is valid
+    while True:
+        guess = input("Please guess a letter: ")
+        if len(guess) == 1 and guess.isalpha():
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guess)
 
 
-while True:
-    guess = input("Please guess a letter: ")
-    if len(guess) == 1 and guess.isalpha():
-        break
+def check_guess(current_guess):
+# format and test the latest input
+    current_guess = current_guess.lower()
+    if current_guess in word:
+        print(f"Good guess! {current_guess} is in the word.")
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+        print(f"Sorry, {current_guess} is not in the word. Try again.")
+
+ask_for_input()
